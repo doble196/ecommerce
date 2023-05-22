7c927a65329388257cfe45773fd0af5230c37bbd
Rensley <87101559+doble196@users.noreply.github.com>
2023-05-22 11:28:22 -0400

    added footer, header and copyright name and date, and NAVBAR

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import logo from "./logo.svg";
+import { Container } from "react-bootstrap";
+
+import Footer from "./components/Footer.js";
+import Header from "./components/Header.js";
 
 function App() {
-  return <div>NFTeria</div>;
+  return (
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to NFTeria</h1>
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
