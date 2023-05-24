d52321c24a25d0e5026fcd9c3d75205df5870f1b
Rensley <87101559+doble196@users.noreply.github.com>
2023-05-24 08:26:59 -0400

    added the name of company

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
     <header>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">NFTeria</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
