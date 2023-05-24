6a130b3eec98b5e09f771c771c9405490280f8d2
Rensley <87101559+doble196@users.noreply.github.com>
2023-05-24 09:08:50 -0400

    added link container for the brand logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,18 +3,26 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
   return (
     <header>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">NFTeria</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>NFTeria</Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i> Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i> Sign In
+              </Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
